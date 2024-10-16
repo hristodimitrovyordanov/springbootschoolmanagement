318d6be423f435e035ad03957e5a79614162e094
Hristo Dimitrov Yordanov <mrhristo29@gmail.com>
2024-10-16 15:12:14 +0300

    Create new School management system with spring

---
+++ src/main/java/com/hristo/springbootmanagementsystem/SpringbootmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.hristo.springbootmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmanagementsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hristo/springbootmanagementsystem/SpringbootmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hristo.springbootmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
